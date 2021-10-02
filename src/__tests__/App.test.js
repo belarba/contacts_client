47b28dbef3c1a968f523123592e0b9dae5a7cbdc
Daniel Belarmino <belarba@gmail.com>
2021-10-02 20:02:52 +0100

    simpliest test file

---
+++ src/__tests__/App.test.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { render } from "@testing-library/react";
+import App from "../App";
+
+// Describe is a test suite
+describe("Testing app functionalities", () => {
+
+// It is an individual test
+  it("renders Contact screen", () => {
+    const { getByText } = render(<App />);
+    const contacts = getByText("Contacts");
+    expect(contacts).toBeVisible();
+  });
+});
